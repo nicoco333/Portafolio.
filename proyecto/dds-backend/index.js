d08bae5d7ae97a27331c3c5c251ebad1eba9dac0
Nicolas <94737@sistemas.frc.utn.edu.ar>
2025-06-17 02:19:18 -0300

    etapa 2 finalizada

--- proyecto/dds-backend/index.js
+++ proyecto/dds-backend/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 
 // crear servidor
 const app = express();
+app.use(express.json()); // para poder leer json en el body
 
 // controlar ruta
 app.get("/", (req, res) => {
     res.send("Backend inicial dds-backend!");
 });
+
+const categoriasmockRouter = require("./routes/categoriasmock");
+app.use(categoriasmockRouter);
+
 
 // levantar servidor
 const port = 3000;
